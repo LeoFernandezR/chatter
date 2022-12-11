b190c3529cadb8f4f268652a0ce13ec8aa0a6c7e
Leonardo <leonardo.fernandez1402@gmail.com>
2022-12-11 17:44:06 -0300

    support dynamic routes

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -11,7 +11,7 @@ const ProtectedRoute = ({children}: {children: React.ReactNode}) => {
     if (!user) {
       router.push({
         pathname: "/",
-        query: {from: router.pathname},
+        query: {from: router.asPath},
       });
     }
   }, [router, user]);
