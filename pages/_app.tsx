7075f7dac08b53e6ac4164590e74a98247dd3a12
Leonardo <leonardo.fernandez1402@gmail.com>
2023-01-03 21:54:34 -0300

    add font and replace react-icons with iconify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,10 @@
 import "../styles/globals.css";
+import "@fontsource/poppins";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/400-italic.css";
+import "@fontsource/poppins/700.css";
+
 import type {AppProps} from "next/app";
 
 import {useRouter} from "next/router";
